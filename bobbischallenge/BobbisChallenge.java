1111d7231e2a3103aca1dcd4d40250f1de10f121
emm-gl <111813459+emm-gl@users.noreply.github.com>
2022-08-30 10:40:56 -0500

    Add files via upload

--- bobbischallenge/BobbisChallenge.java
+++ bobbischallenge/BobbisChallenge.java
@@ -6,25 +6,24 @@ import java.util.*;
 public class BobbisChallenge {
 
     public static void main(String[] args) {
-        // TODO code application logic here
     
     int num;
     Scanner reader = new Scanner(System.in);
-    System.out.println("Welcome to guest the number :P /n You've 8 attempts to guess the number between 1 -100");
+    System.out.println("Welcome to guess the number :P \n You've 8 attempts to guess the number between 1 - 100");
     int rndNumber = (int) (Math.random() * 100);
     
     //Ciclo
         for(int i = 1; i <= 8; i++)
         {
-            System.out.println("Intento número: " + i + "  Ingrese su numero: ");   //"Input the Sphere's radius: ");
+            System.out.println("Attempt number: " + i + "  Input your number: ");   
             num = reader.nextInt();
 
-            if(num == rndNumber)        {System.out.println("Adivino el numero correcto en " + i + " intento(s), se ha ganado un beso de Tzeigou");  return;}
-            if(num < 1 || num > 100)    {System.out.println("El numero ingresado está fuera del rango, intentelo otra vez");}
-            else if(num < rndNumber)         {System.out.println("El numero ingresado es menor que el número secreto, intentelo otra vez");}
-            else if(num > rndNumber)         {System.out.println("El numero ingresado es mayor que el número secreto, intentelo otra vez");}
+            if(num == rndNumber)        {System.out.println("Got it!!! The number was guessed in  " + i + " attempts");  return;}
+            if(num < 1 || num > 100)    {System.out.println("The entered number is out of the range, remember the range is between 0 - 100");}
+            else if(num < rndNumber)         {System.out.println("The entered number is less than the goal");}
+            else if(num > rndNumber)         {System.out.println("The entered number is greater than the goal");}
         }
-        System.out.println("Lo siento no lo logró en los minimos intentos, gracias por nada :P");
+        System.out.println("Sorry, no left attempts the number was: " + rndNumber);
     }
     
 }
